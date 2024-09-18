bdf40d4f64e142c00ec8e9898a759c1725195d63
vaibhavsharma3070 <vaibhavsharma3070@gmail.com>
2024-09-18 19:22:02 +0530

    validate input and validate output

--- midfield/validators.py
+++ midfield/validators.py
@@ -6,7 +6,7 @@ class Midfield:
         self.api_key = api_key
         self.api_url = 'https://api.midfield.ai/api/prompt/'
 
-    def validate(self, prompt):
+    def validate(self, prompt, validate_input, validate_output):
         if not self.api_key:
             raise ValueError("API KEY is required")
 
@@ -16,11 +16,11 @@ class Midfield:
 
         payload = {
             'apikey': self.api_key,
-            'prompt': prompt
+            'prompt': prompt,
+            'validate_input':validate_input,
+            'validate_output':validate_output,
         }
-
         response = requests.post(self.api_url, json=payload, headers=headers)
-
         if response.status_code != 200:
             raise Exception(f"Error: {response.json().get('error')}")
 
